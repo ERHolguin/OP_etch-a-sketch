e6c8c77c046e3955a7522780e141a8782cfab21d
ERHolguin <rrodshedale@gmail.com>
2023-10-19 16:01:33 +0000

    Create Grids

---
+++ pageone.js
@@ -0,0 +1,16 @@
+const rows = 16;
+const columns = 16;
+
+const gridContainer = document.getElementById('container');
+
+for (let i = 0; i < rows; i++) {   
+    for (let j = 0; j < columns; j++) {
+        const singleCell = document.createElement('div');
+        singleCell.className = 'singleCell';
+        singleCell.textContent = ('');
+
+        gridContainer.appendChild(singleCell);
+    }
+}
+    
+
