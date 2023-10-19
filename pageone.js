4e5d40521fa1ec614df786c003c2fc9ac6180fb6
ERHolguin <rrodshedale@gmail.com>
2023-10-19 20:42:59 +0000

    Keep Grids Square No Matter the Size

--- pageone.js
+++ pageone.js
@@ -1,7 +1,7 @@
 const rows = 16;
 const columns = 16;
 
-const gridContainer = document.getElementById('container');
+const gridContainer = document.getElementById('main-grid');
 
 for (let i = 0; i < rows; i++) {   
     for (let j = 0; j < columns; j++) {
